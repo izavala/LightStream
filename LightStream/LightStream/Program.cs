14c776ae08a3641a22198616214c8fbb12f137d7
izavala <isaac.zavala@petabridge.com>
2019-02-25 16:31:33 -0600

    Setup Actors

---
+++ LightStream/LightStream/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Akka.Remote;
+
+namespace LightStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LightStream/LightStream/ReadActor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Akka.Actor;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LightStream
+{
+    class ReadActor : UntypedActor
+    {
+
+        protected override void OnReceive(object message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
